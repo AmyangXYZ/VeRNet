9db3b4015a9ecf9a091084691018de1f3b54e034
Jack Medrek <jackmedrek@jacks-mbp-2.lan>
2023-06-29 12:45:29 -0400

    centered elements in settings component, added icons

--- src/components/SettingsPanel.vue
+++ src/components/SettingsPanel.vue
@@ -18,20 +18,55 @@ watch(
 
 <template>
   <el-card class="card" v-show="SignalShowSettings">
-    <span>Topo Rand Seed </span>
-    <el-input-number v-model="Network.TopoConfig.value.seed" />
-    <br />
-    <span>Num Nodes</span>
-    <el-input-number v-model="Network.TopoConfig.value.num_nodes" :min="1" :max="100" />
-    <br />
-    <span>Transmission Range</span>
-    <el-input-number v-model="Network.TopoConfig.value.tx_range" :min="1" :max="40" />
-    <br />
-    <span>Num Slots</span>
-    <el-input-number v-model="Network.SchConfig.value.num_slots" :min="1" :max="10" />
-    <br />
-    <span>Num Channels</span>
-    <el-input-number v-model="Network.SchConfig.value.num_channels" :min="1" :max="10" />
+    <el-row class="form-row">
+      <el-col :span="12" class="label-col">
+        <img src="../assets/settings-toporand.png" class="icon" />
+        <span class="label">Topo Rand Seed</span>
+      </el-col>
+      <el-col :span="12">
+        <el-input-number v-model="Network.TopoConfig.value.seed" class="input-number" />
+      </el-col>
+    </el-row>
+    
+    <el-row class="form-row">
+      <el-col :span="12" class="label-col">
+        <img src="../assets/settings-numnode.png" class="icon" />
+        <span class="label">Num Nodes</span>
+      </el-col>
+      <el-col :span="12">
+        <el-input-number v-model="Network.TopoConfig.value.num_nodes" :min="1" :max="100" class="input-number" />
+      </el-col>
+    </el-row>
+
+    <el-row class="form-row">
+      <el-col :span="12" class="label-col">
+        <img src="../assets/settings-trange.png" class="icon" />
+        <span class="label">Transmission Range</span>
+      </el-col>
+      <el-col :span="12">
+        <el-input-number v-model="Network.TopoConfig.value.tx_range" :min="1" :max="40" class="input-number" />
+      </el-col>
+    </el-row>
+    
+    <el-row class="form-row">
+      <el-col :span="12" class="label-col">
+        <img src="../assets/settings-numslot.png" class="icon" />
+        <span class="label">Num Slots</span>
+      </el-col>
+      <el-col :span="12">
+        <el-input-number v-model="Network.SchConfig.value.num_slots" :min="1" :max="10" class="input-number" />
+      </el-col>
+    </el-row>
+    
+    <el-row class="form-row">
+      <el-col :span="12" class="label-col">
+        <img src="../assets/settings-numchannel.png" class="icon" />
+        <span class="label">Num Channels</span>
+      </el-col>
+      <el-col :span="12">
+        <el-input-number v-model="Network.SchConfig.value.num_channels" :min="1" :max="10" class="input-number" />
+      </el-col>
+    </el-row>
   </el-card>
 </template>
 
@@ -40,5 +75,32 @@ watch(
   background-color: rgba(0, 0, 0, 0.33);
   height: 100%;
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.form-row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.label-col {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+.label {
+  margin-right: 10px;
+  white-space: nowrap;
+}
+.icon {
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+  filter: brightness(0) invert(1);
+}
+.input-number {
+  margin-left: 10px;
 }
 </style>
